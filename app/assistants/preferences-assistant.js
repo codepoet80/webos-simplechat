2ad76ec3315226f9edb480be0cf117f82261e2b9
CodePoet80 <jonathan.m.wise@gmail.com>
2021-04-12 14:10:49 -0400

    more background check times

--- app/assistants/preferences-assistant.js
+++ app/assistants/preferences-assistant.js
@@ -36,6 +36,7 @@ PreferencesAssistant.prototype.setup = function() {
             label: $L("Background Update"),
             choices: [
                 { label: "Off", value: -1 },
+                { label: "15 minutes", value: "00:15:00" },
                 { label: "30 minutes", value: "00:30:00" },
                 { label: "1 Hour", value: "01:00:00" },
                 { label: "2 Hours", value: "02:00:00" },
