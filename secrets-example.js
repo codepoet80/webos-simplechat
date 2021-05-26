76149a40acef1b1008a4b197e392677b87b701de
CodePoet80 <jonathan.m.wise@gmail.com>
2021-05-26 19:10:25 -0400

    add example

---
+++ secrets-example.js
@@ -0,0 +1,4 @@
+var appKeys = {
+    'shareBoardClientKey': 'base64 encode the client key from the server',
+    'shareBoardCreateKey': 'base64 encode the create key from the server (if needed)'
+};
